700dd3094dd43ef4d80f40b439df4280dcf22382
CzarXIV <onyedikachukwuoscar@gmail.com>
2025-05-02 08:26:15 +0100

    properly set all social media icons in the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Instagram, Facebook, Twitter } from 'lucide-react';
+import { Instagram, Facebook, Twitter, Pinterest, Youtube } from 'lucide-react';
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
